86c8f0fc82bc5d92d402541e9cb8f3aba823ccf6
pantczak <aantczak.secure@gmail.com>
2021-05-31 21:35:31 +0200

    added swagger documentation, added ReadinessController

--- src/main/java/pl/zzpj/spacer/config/SecurityConfig.java
+++ src/main/java/pl/zzpj/spacer/config/SecurityConfig.java
@@ -24,6 +24,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtRequestFilter jwtRequestFilter;
 
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            // other public endpoints of your API may be appended to this array
+            "/readiness"
+    };
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoderBean());
@@ -34,10 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/authentication-succeeded").permitAll()
                 .antMatchers("/authentication-failed").permitAll()
+                .antMatchers("/api-docs").permitAll()
+                .antMatchers("/api-docs/swagger-ui-custom.html").permitAll()
                 .anyRequest().authenticated()
                 .and().logout().permitAll()
                 .and().sessionManagement()
