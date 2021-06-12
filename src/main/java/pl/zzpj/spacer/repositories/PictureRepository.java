db7e3b678b746ff9b400f53d896c761ca6d28c52
pantczak <aantczak.secure@gmail.com>
2021-06-12 20:27:43 +0200

    minor fixes

--- src/main/java/pl/zzpj/spacer/repositories/PictureRepository.java
+++ src/main/java/pl/zzpj/spacer/repositories/PictureRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.zzpj.spacer.model.Picture;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public interface PictureRepository extends MongoRepository<Picture, UUID> {
 
-    List<Picture> findAllByAddDateBetween(LocalDateTime start, LocalDateTime end);
+    List<Picture> findAllByCreationDateBetween(Date start, Date end);
 
     List<Picture> findAllByTagsContaining(String tag);
 }
