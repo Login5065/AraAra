d500c09c946702f6e660261e2873e93a670aae43
betonowy <bl000dy@o2.pl>
2021-06-12 17:51:27 +0200

    wip

---
+++ src/test/java/pl/zzpj/spacer/CommentServiceTest.java
@@ -0,0 +1,25 @@
+package pl.zzpj.spacer;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CommentServiceTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Order(1)
+    @Test
+    void CreateTestData() {
+
+    }
+}
